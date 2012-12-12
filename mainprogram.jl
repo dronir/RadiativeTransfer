92f61a9e34bf08a79ffced25ac5a560a1a4a475a
Olli Wilkman <olli.wilkman@iki.fi>
2012-12-12 15:37:08 +0200

    More versatile main program

--- mainprogram.jl
+++ mainprogram.jl
@@ -2,18 +2,21 @@
 require("radtrans.jl")
 using RadiativeTransfer
 
-function main(N::Integer)
+function main(Nrays::Integer, tau::Float64, omega::Float64, g::Float64)
 	scattered_rays = Array(Ray, N)
+
+	omega = 0.9
 
 	println("\nTracing $N rays...")
 	tic()
-	for i = 1:N
-		scattered_rays[i] = trace_ray(2.0, 0.9, 0.6)
+	for i = 1:Nrays
+		scattered_rays[i] = trace_ray(tau, omega, g)
 	end
 	toc()
 	
 	scattered_intensity = zeros(31)
 	scattering_angles = zeros(N)
+	scattering_orders = zeros(N)
 	I_scattered = 0.0
 	for i = 1:N
 		ray = scattered_rays[i]
@@ -22,6 +25,7 @@ function main(N::Integer)
 		scattering_angles[i] = theta
 		exit_bin = int((theta / pi) * 30) + 1
 		scattered_intensity[exit_bin] += ray.intensity
+		scattering_orders[i] = ceil(-1/log(scattered_rays[i].intensity, 2))
 	end
 	
 
@@ -31,8 +35,11 @@ function main(N::Integer)
 	csvwrite("angles.txt", scattering_angles)
 	println("Wrote scattering angles to angles.txt.")
 	csvwrite("intensity.txt", scattered_intensity)
+	csvwrite("orders.txt", scattering_orders)
 	println("Wrote scattering intensity to intensity.txt.")
 	println("")
 end
 
-main(10000)
+main(N::Integer) = main(N, 2.0, 0.9, 0.5)
+
+#main(10000)
