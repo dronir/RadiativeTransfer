c23bdfd97eb9be09c39bfe41af5be743810b0155
Olli Wilkman <olli.wilkman@iki.fi>
2012-12-18 16:21:00 +0200

    Improve plot script
    Plot script can now optionally take a directory on the command line,
    and find the data in that directory.

--- plot.py
+++ plot.py
@@ -3,10 +3,17 @@
 
 from matplotlib import pyplot as plot
 from numpy import loadtxt, degrees, linspace, sin, cos, pi
+from sys import argv
+from os.path import join
 
-angles = loadtxt("angles.txt")
-intensity = loadtxt("intensity.txt")
-orders = loadtxt("orders.txt")
+if len(argv) > 1:
+    base = argv[1]
+else:
+    base = "."
+
+angles = loadtxt(join(base, "angles.txt"))
+intensity = loadtxt(join(base, "intensity.txt"))
+orders = loadtxt(join(base, "orders.txt"))
 
 T = linspace(0,180.0,31)
 
