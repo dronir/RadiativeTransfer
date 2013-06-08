8ebc433f46d027d52135b6c5dfefd5f435633ee8
Olli Wilkman <olli.wilkman@iki.fi>
2013-06-08 21:17:03 +0300

    Normalize plot window to total intensity

--- plot_sphere_output.py
+++ plot_sphere_output.py
@@ -14,7 +14,7 @@ analytic = data[:,3]
 plot(alpha, first, color="red", linewidth=2, label="first-order")
 plot(alpha, total, color="blue", linewidth=2, label="total")
 plot(alpha, analytic, "--", color="black", linewidth=2, label="analytical")
-ylim(0,1)
+ylim(0,total.max())
 xlim(0,pi)
 legend()
 show()
